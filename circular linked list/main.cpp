135d640aaf6e5998f7d4422acdf1cfd84300ef70
RehanGith <chrehan1007@gmail.com>
2024-03-13 13:31:42 +0500

    Add project files.

---
+++ circular linked list/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+class node {
+public:
+	int data;
+	node* next;
+	node(int num = 0) : data(num) , next(nullptr) {
+
+	}
+};
+void insert_at_head(node*& head, int n) {
+	
+	if (head == nullptr) {
+		head = new node(n);
+		head->next = head;
+		return;
+	}
+	node* nnode = new node(n);
+	head->next = nnode;
+	nnode->next = head;
+	head = nnode;
+}
+void add_at_end(node*& head, int n) {
+	if (head == nullptr) {
+		head = new node(n);
+		head->next = head;
+		return;
+	}
+	node* nnode = new node(n), *temp = head;
+	while (temp->next != head) {
+		temp = temp->next;
+	}
+	temp->next = nnode;
+	nnode->next = head;
+}
+void display(node* head) {
+	node* temp = nullptr;
+	temp = head;
+	do{
+		cout << temp->data << endl;
+		temp = temp->next;
+	} while (temp != head);
+}
+int main() {
+	node* head = nullptr;
+	insert_at_head(head, 1);
+	insert_at_head(head, 2);
+	add_at_end(head, 3);
+	display(head);
+}
