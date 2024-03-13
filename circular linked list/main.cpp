a740bb314390794a81bb1231d33ad4ca66b1b8cf
RehanGith <chrehan1007@gmail.com>
2024-03-13 13:47:30 +0500

    add search function

--- circular linked list/main.cpp
+++ circular linked list/main.cpp
@@ -8,6 +8,18 @@ public:
 
 	}
 };
+int search(node*& head, int sinfo) {
+	node* temp = head;
+	int n{1};
+	do {
+		if (temp->data == sinfo)
+			return n;
+		n++;
+		temp = temp->next;
+	} while (temp != head);
+	return false;
+}
+
 void insert_at_head(node*& head, int n) {
 	
 	if (head == nullptr) {
@@ -33,6 +45,7 @@ void add_at_end(node*& head, int n) {
 	temp->next = nnode;
 	nnode->next = head;
 }
+
 void display(node* head) {
 	node* temp = nullptr;
 	temp = head;
